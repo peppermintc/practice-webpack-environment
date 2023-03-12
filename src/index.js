bf5079a70e43f0adb32466a0c166d2134222c5cc
Hamin Park <haminjp@naver.com>
2023-03-12 19:52:40 +0900

    feat: webpack + react 환결 설정

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Tryout Webpack");
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "none",
+
   entry: "./src/index.js",
+
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+
+  module: {
+    rules: [
+      // JSX 문법 호환
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    // 브라우저 환경에서 process 객체를 사용, 없으면 오류
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+
+    // HTML 파일 처리
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    open: true,
+  },
+
+  resolve: {
+    // 다음 확장자 파일들을 모듈로 인식하도록 설정
+    extensions: [".js", ".jsx"],
+  },
 };
