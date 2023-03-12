d9cee365a7bfb304afbd74dd63f166ac379354f4
Hamin Park <haminjp@naver.com>
2023-03-12 20:18:14 +0900

    Webpack + React 환경 설정 (#4)
    * feat: html-webpack-plugin 추가
    
    * feat: html-webpack-plugin > template 추가
    
    * feat: webpack + react 환결 설정
    
    * refactor: port 넘버 변수로 선언
    
    * docs: 웹팩 + 리액트 환경 설정 설명 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,53 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const DEV_SERVER_PORT = 3000;
 
 module.exports = {
   mode: "none",
+
   entry: "./src/index.js",
+
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      // JSX 문법 호환
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    // 브라우저 환경에서 process 객체를 사용, 없으면 오류
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+
+    // HTML 파일 처리
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+
+  devServer: {
+    host: "localhost",
+    port: DEV_SERVER_PORT,
+    open: true,
+  },
+
+  resolve: {
+    // 다음 확장자 파일들을 모듈로 인식하도록 설정
+    extensions: [".js", ".jsx"],
   },
 };
