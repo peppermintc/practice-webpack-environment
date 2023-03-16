c84ef43579993dc37d263107835edede827734d5
Hamin Park <haminjp@naver.com>
2023-03-17 01:30:35 +0900

    feat: styled-components 추가

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./index.css";
+import styled from "styled-components";
 
 interface Props {
   text: string;
 }
 
-const Title = ({ text }: Props) => <h1 className="title">{text}</h1>;
+const Container = styled.h1`
+  background-color: green;
+`;
+
+const Title = ({ text }: Props) => (
+  <Container className="title">{text}</Container>
+);
 
 export default Title;
